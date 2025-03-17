3afd73f76a85d45078b33fcfca6e1f964d712096
Mrinal P R <94679425+mrinalpravi@users.noreply.github.com>
2025-03-17 10:52:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
-
 resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
+  name        = "jenkins-sg-sre"
   description = "Security group for Jenkins Server"
-  
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -18,29 +24,47 @@ resource "aws_security_group" "jenkins_sg" {
   }
 }
 
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.key_pair.private_key_pem
+  filename        = "${path.module}/sre-key.pem"
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "sre-key"
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
 resource "aws_instance" "jenkins" {
-  ami           = "ami-085ad6ae776d8f09c"
-  instance_type = "t2.small"  
-  key_name      = "mrinal"  
-  
-  security_groups = [aws_security_group.jenkins_sg.name]
+  ami                    = "ami-085ad6ae776d8f09c"
+  instance_type          = "t2.medium"
+  key_name               = aws_key_pair.generated_key.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  root_block_device {
+    volume_size = 32
+    volume_type = "gp3"
+  }
 
   user_data = <<-EOF
     #!/bin/bash
+    sudo dnf upgrade -y
+    sudo dnf install java-17-amazon-corretto -y
     sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-    sudo yum upgrade -y
-    sudo dnf install java-17-amazon-corretto -y
-    sudo yum install jenkins -y
+    sudo dnf install jenkins -y
     sudo systemctl enable jenkins
     sudo systemctl start jenkins
   EOF
 
   tags = {
-    Name = "JenkinsServer"
-    Owner = "Mrinal"
+    Name    = "JenkinsServerSRE"
+    Owner   = "SRE"
     Managed = "Terraform"
   }
 }
-
-
