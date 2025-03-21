9f5be66b8e68f6f1d03cea784dc446c892cff7a5
Mrinal P R <94679425+mrinalpravi@users.noreply.github.com>
2025-03-21 12:53:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "utility-bucket-cloud-practice"
+    key    = "terraform-backend/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
-  region = "us-east-1" 
+  default_tags {
+    tags = {
+      Name        = "JenkinsServerSRE"
+      Owner       = "Mrinal Ravi"
+      Project     = "CICD"
+      Environment = "IT-RnD"
+      CostCenter  = "CDx"
+      Client      = "CloudPractice"
+    }
+  }
+  region = "us-east-1"
 }
